af4085c1c3619008846f86a6746f394f470da5b0
dotaljyajxf <542730772@qq.com>
2020-08-01 16:54:22 +0800

    modidy

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 )
 
 func main() {
-	d := dht.NewDHT("0.0.0.0:12121", 1000)
+	if len(os.Args) != 2 {
+		fmt.Println("need arg limit")
+		return
+	}
+	limit, _ := strconv.Atoi(os.Args[1])
+	d := dht.NewDHT("0.0.0.0:12121", limit)
 	err := d.Start()
 
 	if err != nil {
